0a1a97868dd2dea74396f48d8fca0d08941bb10d
sandraanghel <1815743@student.uwtsd.ac.uk>
2022-06-06 15:44:13 +0100

    Javascript variables - const and let keywords

--- part1/src/App.js
+++ part1/src/App.js
@@ -20,6 +20,16 @@ const Footer = () => {
 const App = () => {
   const name = "Peter";
   const age = 10;
+
+  const x = 1;
+  let y = 5;
+  console.log(x, y);
+  y += 10;
+  console.log(x, y);
+  y = "some text";
+  console.log(x, y);
+  // x = 4;  this causes an error because x was defined with the keywork const which makes it a constant and its value can't be changed
+
   return (
     <>
       <h1>Greetings</h1>
